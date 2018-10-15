6e3c1a2d162e263b44298598ffc3def823ceea15
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-15 14:03:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,8 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  # map to database table 
+  # .create_table
+  # .drop_table
+  # #save_instance_to_table
+  # #create_and_save_instance_to_table
 end
