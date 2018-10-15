9cea2ce2d0831dee8388047426297e7bf800d8c2
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-15 14:06:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,5 +7,10 @@ class Student
   # .create_table
   # .drop_table
   # #save_instance_to_table
-  # #create_and_save_instance_to_table
+  # #create_and_save_instance_to_table 
+  
+  # build out the Student Class such that it has name and grade attributes and optionl id
+  
+  
+  
 end
