3448c31e2fc04d32fffa2a589e498b1695ed6c3d
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-15 14:31:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,4 @@
 class Student
-  # connection to database: DB[:conn]
-  
-  # build out the Student class such that it has name and grade attributes and optionl id
   attr_accessor :name, :grade
   attr_reader   :id
   
@@ -10,8 +7,7 @@ class Student
     @grade = grade
     @id    = id 
   end
-  
-  # .create_table
+
   def self.create_table
     sql =  <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
@@ -22,16 +18,14 @@ class Student
         SQL
     DB[:conn].execute(sql)
   end 
-  
-  # .drop_table
+
   def self.drop_table
     sql = <<-SQL
       DROP TABLE students
       SQL
     DB[:conn].execute(sql)
   end 
-  
-  # #save_instance_to_table
+
   def save 
     sql = <<-SQL
       INSERT INTO students (name, grade)
@@ -41,7 +35,7 @@ class Student
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
-  # #create_and_save_instance_to_table 
+
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save 
