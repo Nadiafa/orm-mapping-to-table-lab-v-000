52d74f3e38661c2ab81afe15b656d84192d915bf
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-15 14:07:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,15 +1,16 @@
 class Student
   # connection to database: DB[:conn]
   
-  # build out the Student class such that it has 
+  # build out the Student class such that it has name and grade attributes and optionl id
   
   # map to database table 
   # .create_table
   # .drop_table
   # #save_instance_to_table
   # #create_and_save_instance_to_table 
-  
-  # build out the Student Class such that it has name and grade attributes and optionl id
+
+
+  attr_accessor :name, :grade, :id
   
   
   
